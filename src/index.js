17430010783716a684996ee26cbb5588f23a57fc
banban-22 <a1ya1k2a9@gmail.com>
2022-07-19 22:39:05 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Alternatives from './Alternatives';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Alternatives />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
